865a12fb2a6fbf9c141e06bf4d4e2566b049b6a5
Kiet Chau <chaukietck25@gmail.com>
2023-11-18 17:02:53 +0700

    update search screen

---
+++ app/src/main/java/com/example/blitz/PersonModel.java
@@ -0,0 +1,29 @@
+package com.example.blitz;
+
+public class PersonModel {
+    private String name;
+    private String email;
+
+    public PersonModel(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
--- app/src/main/java/com/example/blitz/SearchActivity.java
+++ app/src/main/java/com/example/blitz/SearchActivity.java
@@ -1,36 +1,36 @@
 package com.example.blitz;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
-    ArrayAdapter<String> adapter;
+    ArrayList<PersonModel> personList;
+
+    RecyclerView recyclerView;
+    PersonAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        SearchView searchView = findViewById(R.id.nameSearch);
+        recyclerView = findViewById(R.id.recyclerView);
 
-        String[] messages = {"Gia", "Dang", "Hau", "Kiet"};
+        personList = new ArrayList<>();
+        personList.add( new PersonModel( "Dang", "phamdang@gmail.com"));
+        personList.add( new PersonModel( "Hau", "phuchau@gmail.com"));
+        personList.add( new PersonModel( "Kiet", "chaukiet@gmail.com"));
+        personList.add( new PersonModel( "Gia", "hoanggia@gmail.com"));
 
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, messages);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-                return false;
-            }
-        });
+        adapter = new PersonAdapter(this, personList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
